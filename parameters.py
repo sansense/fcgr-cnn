08a6f1991c6b9ac17f96cd977e8c513d04e2bfe6
Santosh Anand <s.anand@campus.unimib.it>
2022-03-13 23:18:18 +0100

    parameters changed for short read (fastq) options

--- parameters.py
+++ parameters.py
@@ -5,6 +5,10 @@ SET PARAMETERS FOR ALL STEPS
 # General
 KMER = 8
 SPECIE = "hCoV-19"
+USE_R1 = True 
+USE_R2 = False # or False
+
+
 
 # Undersample sequences
 PATH_METADATA = "/data/GISAID/metadata.tsv"
@@ -15,7 +19,7 @@ PATH_FASTA_GISAID = "/data/GISAID/sequences.fasta"
 # For training
 TRAIN_SIZE = 0.8 # size for val and test sets = (1-TRAIN_SIZE)/2
 BATCH_SIZE = 16
-EPOCHS = 50
+EPOCHS = 20
 MODEL = f"resnet50_{KMER}mers"
 PREPROCESSING = [("divide_by_max",{})] # ("name_function", dict_kwargs)
 WEIGHTS_PATH = None
@@ -30,6 +34,7 @@ PARAMETERS = dict(
     PATH_FASTA_GISAID = PATH_FASTA_GISAID,
     PATH_METADATA = PATH_METADATA, 
     FOLDER_FASTA = f"data/{SPECIE}",
+    FOLDER_FASTQ = f"data/hCoV-19/fastq",
     FOLDER_FCGR = f"fcgr-{KMER}-mer",
     TRAIN_SIZE = TRAIN_SIZE,
     BATCH_SIZE = BATCH_SIZE,
@@ -37,5 +42,8 @@ PARAMETERS = dict(
     MODEL = MODEL,
     PREPROCESSING = PREPROCESSING,
     WEIGHTS_PATH = WEIGHTS_PATH,
-    SEED = 42,    
+    SEED = 42,
+    # for reads
+    USE_R1 = USE_R1,
+    USE_R2 = USE_R2    
 )
